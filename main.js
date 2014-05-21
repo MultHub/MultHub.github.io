e7f6de22518712ddbfd8a968e5dc3ef3b5c936e0
Multi <fgflorianguenter@googlemail.com>
2014-05-21 17:43:37 +0200

    New repository created FROM COMMAND LINE.

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Hello random guy who looks at the JavaScript console.");
+console.log("I guess you have your eyes open right now.");
+console.log("[END OF PRE-RECORDED MESSAGE]");
+console.warn("I am watching you...");
